00f003010b6b1a9704ac8436fe09300a5951beb2
Lzu Tao <taolzu@gmail.com>
2020-03-16 08:30:23 +0700

    Try to fix the linkage of wmemchr on MSVC

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
 #[cfg(not(windows))]
 compile_error!("This crate only works on Windows.");
 
+#[cfg(target_env = "msvc")]
+#[cfg(not(target_feature = "crt-static"))]
+compile_error!("wmemchr in MSVC requires the C runtime to be statically linked");
+
 pub fn wmemchr(needle: u16, haystack: &[u16]) -> Option<usize> {
     extern "C" {
+        #[cfg(target_env = "gnu")]
+        fn wmemchr(s: *const u16, c: u16, n: usize) -> *mut u16;
+    }
+
+    extern "C" {
+        #[cfg(target_env = "msvc")]
+        // FIXME: Hopefully this will fix the "external symbol wmemchr" on MSVC toolchain
+        //        as clang include a builtin function for wmemchr.
+        //
+        // Try to uncomment the line below in case wmemchr symbol cannot be resolved.
+        // #[link_name = "__builtin_wmemchr"]
         fn wmemchr(s: *const u16, c: u16, n: usize) -> *mut u16;
     }
     let len = haystack.len();
