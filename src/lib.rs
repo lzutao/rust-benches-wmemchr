215c60c08d76935441ce7c49cb1baf50cd7816e3
Lzu Tao <taolzu@gmail.com>
2020-03-16 21:25:09 +0700

    Remove wmemchr version as reported by mati865

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,5 @@
-// #[cfg(not(windows))]
-// compile_error!("This crate only works on Windows.");
-
-// #[cfg(target_env = "gnu")]
-// pub fn wmemchr(needle: u16, haystack: &[u16]) -> Option<usize> {
-//     extern "C" {
-//         fn wmemchr(s: *const u16, c: u16, n: usize) -> *mut u16;
-//     }
-
-//     let len = haystack.len();
-//     let ptr = haystack.as_ptr();
-//     let p = unsafe { wmemchr(ptr, needle, len) };
-//     if p.is_null() {
-//         None
-//     } else {
-//         Some((p as usize - ptr as usize) / 2)
-//     }
-// }
+#[cfg(not(windows))]
+compile_error!("This crate only works on Windows.");
 
 // Ported from <https://github.com/bminor/glibc/blob/master/wcsmbs/wmemchr.c>.
 // #[cfg(target_env = "msvc")]
