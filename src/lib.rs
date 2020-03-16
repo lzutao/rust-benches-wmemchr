ce8678d8dce72dcbf00548ed9719f8b3c0eadc50
Lzu Tao <taolzu@gmail.com>
2020-03-16 11:22:41 +0700

    Bench both gnu and msvc

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,12 @@
 #[cfg(not(windows))]
 compile_error!("This crate only works on Windows.");
 
-// See <https://developercommunity.visualstudio.com/content/problem/234075/unresolved-symbols-in-iosfwd-with-has-constexpr-ch-1.html>
-#[cfg(target_env = "msvc")]
-#[cfg(not(target_feature = "crt-static"))]
-compile_error!("wmemchr in MSVC requires the C runtime to be statically linked");
-
+#[cfg(target_env = "gnu")]
 pub fn wmemchr(needle: u16, haystack: &[u16]) -> Option<usize> {
-    #[cfg(target_env = "gnu")]
     extern "C" {
         fn wmemchr(s: *const u16, c: u16, n: usize) -> *mut u16;
     }
 
-    #[cfg(target_env = "msvc")]
-    // #[link(name = "libucrt")]
-    extern "C" {
-        // FIXME: Hopefully this will fix the "external symbol wmemchr" on MSVC toolchain
-        //        as clang include a builtin function for wmemchr.
-        //
-        // Try to uncomment the line below in case wmemchr symbol cannot be resolved.
-        #[link_name = "__builtin_wmemchr"]
-        fn wmemchr(s: *const u16, c: u16, n: usize) -> *mut u16;
-    }
     let len = haystack.len();
     let ptr = haystack.as_ptr();
     let p = unsafe { wmemchr(ptr, needle, len) };
@@ -29,5 +14,40 @@ pub fn wmemchr(needle: u16, haystack: &[u16]) -> Option<usize> {
         None
     } else {
         Some((p as usize - ptr as usize) / 2)
+    }
+}
+
+#[cfg(target_env = "msvc")]
+pub fn wmemchr(needle: u16, haystack: &[u16]) -> Option<usize> {
+    use std::ffi::c_void;
+    use std::os::raw::c_int;
+    extern "C" {
+        fn memchr(s: *const c_void, c: c_int, n: usize) -> *mut c_void;
+    }
+
+    let [low, high] = needle.to_le_bytes();
+    let mut ptr = haystack.as_ptr() as *const u8;
+    let mut len = haystack.len() * 2;
+    let mut start = 0;
+
+    loop {
+        let p = unsafe { memchr(ptr as *const c_void, low as c_int, len) } as *const u8;
+        if p.is_null() {
+            return None;
+        }
+
+        let off = p as usize - ptr as usize;
+        if off >= len {
+            return None;
+        }
+
+        start += off;
+        if unsafe { *p.add(1) == high } {
+            assert!(start % 2 == 1);
+            return Some((start + 1) / 2);
+        }
+
+        len -= off;
+        ptr = unsafe { ptr.add(off) };
     }
 }
