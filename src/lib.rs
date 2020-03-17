67319e01e50d76e71539fae105aef7b3ddf6f71a
Lzu Tao <taolzu@gmail.com>
2020-03-17 09:16:46 +0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -4,56 +4,21 @@
 // Ported from <https://github.com/bminor/glibc/blob/master/wcsmbs/wmemchr.c>.
 pub fn unrolled_find_u16s(needle: u16, haystack: &[u16]) -> Option<usize> {
     let ptr = haystack.as_ptr();
+    const RATIO: usize = core::mem::size_of::<u16>();
 
     // For performance reasons unfold the loop eight times.
     let mut chunks_8 = haystack.chunks_exact(8);
     for chunk in &mut chunks_8 {
-        let mut iter = chunk.iter();
-        if let Some(c) = iter.next() {
+        for c in chunk {
             if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / 2);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / 2);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / 2);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / 2);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / 2);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / 2);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / 2);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / 2);
+                return Some((c as *const u16 as usize - ptr as usize) / RATIO);
             }
         }
     }
 
     for c in chunks_8.remainder() {
         if *c == needle {
-            return Some((c as *const u16 as usize - ptr as usize) / 2);
+            return Some((c as *const u16 as usize - ptr as usize) / RATIO);
         }
     }
     None
