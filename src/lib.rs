d3aa40761612c626d04aa15bf644f45329c79768
Lzu Tao <taolzu@gmail.com>
2020-03-17 09:16:46 +0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -61,15 +61,18 @@ pub fn unrolled_find_u16s(needle: u16, haystack: &[u16]) -> Option<usize> {
 
 #[test]
 fn test_implementation() {
-    let r = unrolled_find_u16s(0, &[1, 2, 3, 4]);
+    let r = unrolled_find_u16s(0, &[1, 2, 3, 4, 5, 6, 7, 8, 9]);
     assert_eq!(r, None);
 
-    let r = unrolled_find_u16s(0, &[0, 2, 3, 4]);
+    let r = unrolled_find_u16s(0, &[0, 2, 3, 4, 5, 6, 7, 8, 9]);
     assert_eq!(r, Some(0));
 
-    let r = unrolled_find_u16s(0, &[1, 2, 0, 4]);
+    let r = unrolled_find_u16s(0, &[1, 2, 0, 4, 5, 6, 7, 8, 9]);
     assert_eq!(r, Some(2));
 
-    let r = unrolled_find_u16s(0, &[1, 2, 3, 0]);
-    assert_eq!(r, Some(3));
+    let r = unrolled_find_u16s(0, &[1, 2, 3, 4, 5, 6, 7, 0, 9]);
+    assert_eq!(r, Some(7));
+
+    let r = unrolled_find_u16s(0, &[1, 2, 3, 4, 5, 6, 7, 8, 0]);
+    assert_eq!(r, Some(8));
 }
