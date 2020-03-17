170356cd717e4e9a6137e55f331831caf6155567
Lzu Tao <taolzu@gmail.com>
2020-03-17 09:16:46 +0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub fn unrolled_find_u16s(needle: u16, haystack: &[u16]) -> Option<usize> {
     // For performance reasons unfold the loop eight times.
     let mut chunks_8 = haystack.chunks_exact(8);
     for chunk in chunks_8 {
-        let iter = chunk.iter().enumerate();
+        let mut iter = chunk.iter().enumerate();
         if let Some((i, c)) = iter.next() {
             if *c == needle {
                 return Some((c as *const u16 as usize - ptr as usize) / 2);
