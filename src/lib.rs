dc8553dc6f08562d180885878e660d2cac2a07b4
Lzu Tao <taolzu@gmail.com>
2020-03-17 09:16:46 +0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -4,47 +4,87 @@
 // Ported from <https://github.com/bminor/glibc/blob/master/wcsmbs/wmemchr.c>.
 pub fn unrolled_find_u16s(needle: u16, haystack: &[u16]) -> Option<usize> {
     let ptr = haystack.as_ptr();
-    let mut len = haystack.len();
-    let mut start = &haystack[..];
 
     // For performance reasons unfold the loop eight times.
     let mut chunks_8 = haystack.chunks_exact(8);
     for chunk in chunks_8 {
-        let mut iter = chunk.iter().enumerate();
-        if let Some((i, c)) = iter.next() {
+        let mut iter = chunk.iter();
+        if let Some(c) = iter.next() {
             if *c == needle {
                 return Some((c as *const u16 as usize - ptr as usize) / 2);
             }
         }
-        if let Some((i, c)) = iter.next() {
+        if let Some(c) = iter.next() {
             if *c == needle {
                 return Some((c as *const u16 as usize - ptr as usize) / 2);
             }
         }
-        if let Some((i, c)) = iter.next() {
+        if let Some(c) = iter.next() {
             if *c == needle {
                 return Some((c as *const u16 as usize - ptr as usize) / 2);
             }
         }
-        if let Some((i, c)) = iter.next() {
+        if let Some(c) = iter.next() {
             if *c == needle {
                 return Some((c as *const u16 as usize - ptr as usize) / 2);
             }
         }
-        if let Some((i, c)) = iter.next() {
+        if let Some(c) = iter.next() {
             if *c == needle {
                 return Some((c as *const u16 as usize - ptr as usize) / 2);
             }
         }
-        if let Some((i, c)) = iter.next() {
+        if let Some(c) = iter.next() {
             if *c == needle {
                 return Some((c as *const u16 as usize - ptr as usize) / 2);
             }
         }
-        if let Some((i, c)) = iter.next() {
+        if let Some(c) = iter.next() {
             if *c == needle {
                 return Some((c as *const u16 as usize - ptr as usize) / 2);
             }
+        }
+        if let Some(c) = iter.next() {
+            if *c == needle {
+                return Some((c as *const u16 as usize - ptr as usize) / 2);
+            }
+        }
+    }
+
+    let mut remains = chunks_8.remainder().iter();
+    if let Some(c) = remains.next() {
+        if *c == needle {
+            return Some((c as *const u16 as usize - ptr as usize) / 2);
+        }
+    }
+    if let Some(c) = remains.next() {
+        if *c == needle {
+            return Some((c as *const u16 as usize - ptr as usize) / 2);
+        }
+    }
+    if let Some(c) = remains.next() {
+        if *c == needle {
+            return Some((c as *const u16 as usize - ptr as usize) / 2);
+        }
+    }
+    if let Some(c) = remains.next() {
+        if *c == needle {
+            return Some((c as *const u16 as usize - ptr as usize) / 2);
+        }
+    }
+    if let Some(c) = remains.next() {
+        if *c == needle {
+            return Some((c as *const u16 as usize - ptr as usize) / 2);
+        }
+    }
+    if let Some(c) = remains.next() {
+        if *c == needle {
+            return Some((c as *const u16 as usize - ptr as usize) / 2);
+        }
+    }
+    if let Some(c) = remains.next() {
+        if *c == needle {
+            return Some((c as *const u16 as usize - ptr as usize) / 2);
         }
     }
     None
