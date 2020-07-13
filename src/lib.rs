18d6d3a0dbf7f5ba18d1a458f4da0b6aa17cdbd2
Lzu Tao <taolzu@gmail.com>
2020-07-13 14:59:36 +0700

    Use macro to reduce repetition

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,26 @@
-const RATIO: usize = core::mem::size_of::<u16>();
+macro_rules! if_return {
+    ($start:ident, $ptr:ident, $needle:ident, $($n:literal,)+) => {
+        $(
+            if $start[$n] == $needle {
+                return Some((&$start[$n] as *const u16 as usize - $ptr as usize) / 2);
+            }
+        )+
+    }
+}
 
 // Ported from <https://github.com/bminor/glibc/blob/master/wcsmbs/wmemchr.c>.
 pub fn unrollled_wmemchr(needle: u16, haystack: &[u16]) -> Option<usize> {
     let ptr = haystack.as_ptr();
-
     // For performance reasons unfold the loop eight times.
-    for chunk in &mut haystack.chunks(8) {
-        let mut iter = chunk.iter();
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / RATIO);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / RATIO);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / RATIO);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / RATIO);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / RATIO);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / RATIO);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / RATIO);
-            }
-        }
-        if let Some(c) = iter.next() {
-            if *c == needle {
-                return Some((c as *const u16 as usize - ptr as usize) / RATIO);
-            }
+    let mut chunks = haystack.chunks_exact(8);
+
+    for chunk in &mut chunks {
+        if_return!(chunk, ptr, needle, 0, 1, 2, 3, 4, 5, 6, 7,);
+    }
+
+    for c in chunks.remainder() {
+        if *c == needle {
+            return Some((c as *const u16 as usize - ptr as usize) / 2);
         }
     }
     None
@@ -58,38 +33,15 @@ pub fn unrollled_wmemchr_iterative(needle: u16, haystack: &[u16]) -> Option<usiz
 
     /* For performance reasons unfold the loop four times.  */
     while len >= 8 {
-        if start[0] == needle {
-            return Some((start.as_ptr() as usize - ptr as usize) / RATIO);
-        }
-        if start[1] == needle {
-            return Some((start[1..].as_ptr() as usize - ptr as usize) / RATIO);
-        }
-        if start[2] == needle {
-            return Some((start[2..].as_ptr() as usize - ptr as usize) / RATIO);
-        }
-        if start[3] == needle {
-            return Some((start[3..].as_ptr() as usize - ptr as usize) / RATIO);
-        }
-        if start[4] == needle {
-            return Some((start[4..].as_ptr() as usize - ptr as usize) / RATIO);
-        }
-        if start[5] == needle {
-            return Some((start[5..].as_ptr() as usize - ptr as usize) / RATIO);
-        }
-        if start[6] == needle {
-            return Some((start[6..].as_ptr() as usize - ptr as usize) / RATIO);
-        }
-        if start[7] == needle {
-            return Some((start[7..].as_ptr() as usize - ptr as usize) / RATIO);
-        }
+        if_return!(start, ptr, needle, 0, 1, 2, 3, 4, 5, 6, 7,);
 
         start = &start[8..];
         len -= 8;
     }
 
-    for (i, c) in start.iter().enumerate() {
+    for c in start {
         if *c == needle {
-            return Some((start.as_ptr() as usize - ptr as usize) / RATIO + i);
+            return Some((c as *const u16 as usize - ptr as usize) / 2);
         }
     }
     None
